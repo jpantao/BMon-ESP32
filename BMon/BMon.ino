e61a0c2a189147068653fa316c75c978642f887c
João Antão <jp.antao@campus.fct.unl.pt>
2021-06-08 23:33:32 +0100

    added basic lum, hum, and temp read functions

---
+++ BMon/BMon.ino
@@ -0,0 +1,95 @@
+
+#include "DHT.h"
+
+// led pins
+const int redLumPin = 6;
+const int greenLumPin = 7;
+const int blueLumPin = 8;
+const int redHumPin = 9;
+const int greenHumPin = 10;
+const int blueHumPin = 11;
+const int redTempPin = 12;
+const int greenTempPin = 13;
+const int blueTempPin = 14;
+const int redMoistPin = 15;
+const int greenMoistPin = 16;
+const int blueMoistPin = 17;
+
+// sensor pins
+const int photoregistorPin = 1;
+const int dht11Pin = 3;
+const int moisturePin = 5;
+
+
+#define DHTTYPE DHT11
+DHT dht(dht11Pin, DHTTYPE); // DHT11
+
+#if defined(ARDUINO_ARCH_AVR)
+    #define debug  Serial
+
+#elif defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_SAM)
+    #define debug  SerialUSB
+#else
+    #define debug  Serial
+#endif
+
+float temp_hum_val[2] = {0};
+int lum_val = 0;
+int moist_val = 0;
+
+void setup() {
+  debug.begin(115200);
+  
+  // leds setup
+  for(int i = 9; i <=17; i++){
+    pinMode(i, OUTPUT);
+  }
+
+  // sensors setup
+  dht.begin();
+
+}
+
+
+void loop() {
+  debug.println("--- Loop START ---");
+
+  
+  // Measurements
+  read_temp_hum();
+  read_lum();
+  
+ 
+  
+  delay(5000); // wait before next cycle
+  debug.println("---- Loop END ----");
+}
+
+void read_temp_hum(){
+  // DHT11 read 
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  if (!dht.readTempAndHumidity(temp_hum_val)) {
+        debug.print("Humidity: ");
+        debug.print(temp_hum_val[0]);
+        debug.print(" %\t");
+        debug.print("Temperature: ");
+        debug.print(temp_hum_val[1]);
+        debug.println(" *C");
+    } else {
+        debug.println("Failed to get temprature and humidity value.");
+    }
+}
+
+
+void read_lum(){
+  lum_val = analogRead(photoregistorPin);
+  lum_val = map(lum_val, 0, 8191, 0, 100);
+  lum_val = constrain(lum_val, 0, 100);
+  debug.print("lum value: ");
+  debug.println(lum_val);
+}
+
+void read_moist(){
+  
+}
